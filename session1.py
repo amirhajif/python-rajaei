40f5a1e9722068bf5fbcd3f5138af2cf59ef118d
amirhossein hajitabar <amirhajitabar2@gmail.com>
2024-07-30 18:30:03 +0330

    Create session1.py

---
+++ session1.py
@@ -0,0 +1,89 @@
+# man dar in ghesmat darmored single,double sohbat kardam
+
+# print("hello world")
+# print('hello world')
+
+'''
+asdhajklshdklajsdasd
+asdklajsdlkjaskldjasd
+aksdljalsdjaklsdklajsd
+'''
+
+# username = "amirhossein"
+# score=15
+# print("username") 
+# print(username) 
+
+# user_name1="amir"
+
+# userName="amir"
+# bestStudentScore=20
+
+# username=input("please enter username: ")
+# print(username)
+
+# username="alireza"
+
+# score=20
+# print(score)
+
+# print("amirhossein",end=" ")
+# print("hajitabar")
+# print("python class")
+
+# print("amirhossein\nhajitabar\tpythonclass")
+
+# uni="shahid rajaji"
+# print(uni)
+# print(type(uni))
+# print(len(uni))
+# print(uni[3])
+# print(uni[6])
+# print(uni[-2])
+# # slicing
+# print(uni[1:5])
+
+
+# language="python"
+# print(language.upper())
+# sentence="i love programming"
+# print(sentence.title())
+# name="AMIR"
+# print(name.lower())
+
+# sentence="i love programming"
+# print(sentence.replace('r','f'))
+
+# products="iphone,macbook,ipad"
+# print(products.split(","))
+# splitedProducst=products.split(",")
+# print(splitedProducst)
+
+# username="   amir"
+# print("user with ",username.strip(),"sign up")
+
+# email=input("please enter email: ")
+# username=input("please enter username: ")
+
+# print(f"user with {email} and {username} signup ")
+# print("user with {} and {} created".format(email,username))
+
+# tol=5
+# arz=4
+# print((tol+arz)*2)
+
+# number=int(input("please enter number: "))
+# print(number*4)
+
+# f=1.8c+32
+
+# degree=float(input("please enter degree: "))
+# f=1.8*degree+32
+# print(f)
+
+# money=1_000_000
+# print(money)
+
+# PI=3.14
+
+# username,password,email="amir",123,"amir@gmail.com"
